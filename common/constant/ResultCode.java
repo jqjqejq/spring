4a137c48f0d93e5d043adb7c17b538e13c2f1718
iiiiiiiii <jqjqejq@yahoo.co.jp>
2018-06-22 14:36:46 +0900

    Create ResultCode.java

---
+++ common/constant/ResultCode.java
@@ -0,0 +1,57 @@
+package jp.co.pmacmobile.common.constant;
+
+/**
+ * @author 71432393
+ *
+ */
+public enum ResultCode {
+
+    /* SUCCESS */
+    SUCCESS(100, "success"),
+
+    /* システムエラー */
+    SYSTEM_INNER_ERROR(500, "システムエラー"),
+
+    BUSINESS_ERROR(101, "businessError");
+
+    private Integer code;
+
+    private String message;
+
+    ResultCode(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer code() {
+        return this.code;
+    }
+
+    public String message() {
+        return this.message;
+    }
+
+    public static String getMessage(String name) {
+        for (ResultCode item : ResultCode.values()) {
+            if (item.name().equals(name)) {
+                return item.message;
+            }
+        }
+        return name;
+    }
+
+    public static Integer getCode(String name) {
+        for (ResultCode item : ResultCode.values()) {
+            if (item.name().equals(name)) {
+                return item.code;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return this.name();
+    }
+
+}
